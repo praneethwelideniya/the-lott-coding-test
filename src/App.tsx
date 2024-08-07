32d234f5543c858b1753983795b6ce1d2e50a895
praneethwelideniya <mpraneethw@gmail.com>
2024-08-08 03:02:47 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="result-container">
+      {/* Header */}
+      <header className="header">
+        <h1>Powerball Results</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export enum ResultType {
+  PRIMARY = "PRIMARY",
+  SECONDARY = "SECONDARY",
+}
+
+export const PRIMARY_NUMBERS = 35;
+export const SECONDARY_NUMBERS = 20;
+export const PRIMARY_WINNING_NUMBERS = 7;
+export const SECONDARY_WINNING_NUMBERS = 1;
+
+export const ThemedColors = {
+  primary: "#610774",
+  secondary: "#007bff",
+  background: "#6c757d",
+  background_2: "#f0f0f0",
+  white: "#ffffff",
+  black: "#000000",
+};
