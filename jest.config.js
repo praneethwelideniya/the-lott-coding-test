ad4c005273359e7b378fdd6fdf2f84ffb32316e4
praneethwelideniya <mpraneethw@gmail.com>
2024-08-08 07:41:26 +1000

    updated unit tests for App

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,18 @@ export default {
     'ts-jest': {
       tsconfig: 'tsconfig.app.json'
     }
-  }
+  },
+  testRegex: "/*.test.(ts|tsx)$",
+  collectCoverage: true,
+  coverageReporters: ["lcov"],
+  coverageDirectory: "test-coverage",
+  coverageThreshold: {
+   "global": {
+   "branches": 0,
+   "functions": 0,
+   "lines": 0,
+   "statements": 0
+   }
+  },
+  moduleDirectories: ["node_modules", "src"]
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,24 +1,39 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
-import {
-  PRIMARY_WINNING_NUMBERS,
-  SECONDARY_WINNING_NUMBERS,
-} from "./constants";
+import { getResults } from "./api";
 
-test("renders Powerball Results header", () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Powerball Results/i);
-  expect(headerElement).toBeInTheDocument();
-});
+jest.mock("./api", () => ({
+  getResults: jest.fn().mockResolvedValueOnce({
+    PrimaryNumbers: [1, 2, 3, 4, 5, 6, 7],
+    SecondaryNumbers: [6],
+  }),
+}));
 
-test("renders primary numbers circles", () => {
-  render(<App />);
-  const primaryNumbers = screen.getAllByTestId("primary-circle");
-  expect(primaryNumbers.length).toBe(PRIMARY_WINNING_NUMBERS);
-});
+describe("App", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-test("renders secondary numbers circles", () => {
-  render(<App />);
-  const secondaryNumbers = screen.getAllByTestId("secondary-circle");
-  expect(secondaryNumbers.length).toBe(SECONDARY_WINNING_NUMBERS);
+  test("renders Powerball Results header", () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Powerball Results/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  test("calls getResults when Flash button is clicked", async () => {
+    render(<App />);
+    const getDataButton = screen.getByTestId("flash-icon");
+    fireEvent.click(getDataButton);
+
+    await waitFor(() => {
+      expect(getResults).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test("resets primary and secondary numbers when Reset Data button is clicked", () => {
+    render(<App />);
+    const resetDataButton = screen.getByTestId("trash-icon");
+    fireEvent.click(resetDataButton);
+    expect(true).toBeTruthy();
+  });
 });
